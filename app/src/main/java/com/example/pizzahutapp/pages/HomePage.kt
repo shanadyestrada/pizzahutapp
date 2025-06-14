eeed98c60c7390cbad01de992e17608b4df8b191
Shanady Estrada Orosco <shanadyestradao@gmail.com>
2025-06-14 16:09:28 -0500

    Carga de imagenes desde Firestore

--- app/src/main/java/com/example/pizzahutapp/pages/HomePage.kt
+++ app/src/main/java/com/example/pizzahutapp/pages/HomePage.kt
@@ -2,23 +2,25 @@ package com.example.pizzahutapp.pages
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.example.pizzahutapp.components.BannerView
+import com.example.pizzahutapp.components.HeaderView
 
 @Composable
 fun HomePage(modifier: Modifier = Modifier) {
-    Column (modifier = Modifier
-        .fillMaxSize()
-        .padding(32.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+    Column (modifier = Modifier.fillMaxSize().padding(16.dp)
     ){
-        Text(text = "Vista del Inicio")
+        HeaderView(modifier)
+        Spacer(modifier = Modifier.height(10.dp))
+        BannerView(modifier = Modifier.height(200.dp))
     }
 
 }
